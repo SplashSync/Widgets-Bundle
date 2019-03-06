e802debbde48ca62547f179282aeff9765d76705
BadPixxel <eshop.bpaquier@gmail.com>
2019-03-06 03:10:43 +0100

    WIP: Grumphp + PhpStan

--- web/app_test.php
+++ web/app_test.php
@@ -1,13 +1,25 @@
 <?php
 
+/*
+ *  This file is part of SplashSync Project.
+ *
+ *  Copyright (C) 2015-2019 Splash Sync  <www.splashsync.com>
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
+
 use Symfony\Component\HttpFoundation\Request;
 
 umask(0002); // This will let the permissions be 0775
 
-// This check prevents access to debug front controllers 
-if (
-        !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1', "147.135.200.130")) 
-        || php_sapi_name() === 'cli-server')
+// This check prevents access to debug front controllers
+if (!(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1', "147.135.200.130"), true)
+        || PHP_SAPI === 'cli-server')
 ) {
     exit('You are not allowed to access this file.');
 }
